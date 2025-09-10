34a2fef3c5ec56d859ef0c2f5df5385e266c74bb
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-10 11:36:33 +0530

    init: notification service with node and test run ✅

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("server is running on port " + PORT);
+});
